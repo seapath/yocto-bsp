aa4c05e72f70b241a27188350f11bf4dbdf29539
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2020-10-08 15:55:38 +0200

    tools/demo_setup: disable rbd exclusive lock
    Ceph’s RADOS Block Devices (RBD) offers a exclusive lock mechanism used
    by Qemu to lock write access to the virtual disks currently used (as it
    would do with a disk file in a regular filesystem).
    During a failover this lock is not gracefully released and therefore
    blocks disk writes until the lock timeout expires.
    
    See https://docs.ceph.com/en/latest/rbd/rbd-exclusive-locks/
    
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

--- tools/demo_setup/root/opt/setup/setup_rbd.sh
+++ tools/demo_setup/root/opt/setup/setup_rbd.sh
@@ -2,3 +2,4 @@
 set -e
 ceph osd pool create rbd 64 8 replicated >/dev/null
 ceph osd pool application enable rbd rbd >/dev/null
+rbd config global set global rbd_default_features 'layering, deep-flatten'
