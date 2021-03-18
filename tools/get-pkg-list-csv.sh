cfe1ded5616ebd67eabbe86b4499fe7d1c850f7f
Philip-Dylan Gleonec <philip-dylan.gleonec@savoirfairelinux.com>
2021-03-18 18:45:59 +0100

    tools/get-pkg-list-csv.sh: output to stdout
    Make the script to generate a csv of all packages output to stdout.
    This enables redirection to any file, instead of hardcoding the output
    file and path.
    
    Issue: #5515
    Signed-off-by: Philip-Dylan Gleonec <philip-dylan.gleonec@savoirfairelinux.com>
    Change-Id: If2227e28d36f4609be92e2efa0ce9f781e60deea

--- tools/get-pkg-list-csv.sh
+++ tools/get-pkg-list-csv.sh
@@ -2,16 +2,15 @@
 # Copyright (C) 2021, RTE (http://www.rte-france.com)
 # SPDX-License-Identifier: Apache-2.0
 
-help() { echo "Usage: $0 [files to add]" 1>&2; exit 1; }
+help() { echo "Usage: $0 [files to add] > /path/to/output/file" 1>&2; exit 1; }
 
 if [ $1 = "-h" ]; then help; fi
 
-OUTFILE="$(date -u +"%Y-%m-%d_%H-%M-%S").csv"
-echo "Name Arch Version Image" > $OUTFILE
+echo "Name Arch Version Image"
 
 for f in $@; do
   IMAGE=${f%%.*}
   IMAGE=${IMAGE%-*}
 
-  sed -e "s/$/ $IMAGE/" $f >> $OUTFILE
+  sed -e "s/$/ $IMAGE/" $f
 done
